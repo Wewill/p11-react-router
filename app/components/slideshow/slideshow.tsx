321b4991a29892e38f4a900611b1eca30a1ef9f3
Wilhem Arnoldy <contact@wilhemarnoldy.fr>
2025-03-19 11:31:00 +0100

    fix: make sure slideshow is looping

--- app/components/slideshow/slideshow.tsx
+++ app/components/slideshow/slideshow.tsx
@@ -8,15 +8,15 @@ export default function Slideshow({ pictures }: SlideshowProps) {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const handlePrev = () => {
-    if (currentIndex > 0) {
-      setCurrentIndex(currentIndex - 1);
-    }
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? pictures.length - 1 : prevIndex - 1
+    );
   };
 
   const handleNext = () => {
-    if (currentIndex < pictures.length - 1) {
-      setCurrentIndex(currentIndex + 1);
-    }
+    setCurrentIndex((prevIndex) =>
+      prevIndex === pictures.length - 1 ? 0 : prevIndex + 1
+    );
   };
 
   return (
@@ -29,22 +29,18 @@ export default function Slideshow({ pictures }: SlideshowProps) {
             alt={`Slide ${currentIndex + 1}`}
           />
         )}
-        {currentIndex > 0 && (
-          <button
-            className="prev absolute left-0 top-50 ms-10 text-6xl text-white [text-shadow:_0_2px_10px_rgb(0_0_0_/_20%)]"
-            onClick={handlePrev}
-          >
-            &#10094;
-          </button>
-        )}
-        {currentIndex < pictures.length - 1 && (
-          <button
-            className="next absolute right-0 top-50 me-10 text-6xl text-white [text-shadow:_0_2px_10px_rgb(0_0_0_/_20%)]"
-            onClick={handleNext}
-          >
-            &#10095;
-          </button>
-        )}
+        <button
+          className="prev absolute left-0 top-50 ms-10 text-6xl text-white [text-shadow:_0_2px_10px_rgb(0_0_0_/_20%)]"
+          onClick={handlePrev}
+        >
+          &#10094;
+        </button>
+        <button
+          className="next absolute right-0 top-50 me-10 text-6xl text-white [text-shadow:_0_2px_10px_rgb(0_0_0_/_20%)]"
+          onClick={handleNext}
+        >
+          &#10095;
+        </button>
         <div className="slideshow-indicator absolute w-full flex bottom-0 mb-2 place-content-center text-white [text-shadow:_0_2px_10px_rgb(0_0_0_/_20%)]">
           {currentIndex + 1} / {pictures.length}
         </div>
